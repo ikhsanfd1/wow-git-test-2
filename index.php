f6ed28595d3d422ffb84bd931badb4ce45c4e470
ikhsanfd1 <dilah80999@gmail.com>
2023-08-27 16:28:55 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 
 <body>
     <h1>Hello Wow!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus aperiam laboriosam dolorum explicabo nihil molestias aliquid ipsa quaerat, neque autem dolore perspiciatis ipsum qui adipisci sunt, delectus ad reprehenderit ullam?</p>
 </body>
 
 </html>
