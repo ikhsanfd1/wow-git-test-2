17e376215aa93e1405bb0154db8413e3d871b83b
ikhsanfd1 <dilah80999@gmail.com>
2023-08-27 16:34:35 +0700

    menambahkan list baru

--- index.php
+++ index.php
@@ -9,6 +9,11 @@
 
 <body>
     <h1>Hello Wow!</h1>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus aperiam laboriosam dolorum explicabo nihil molestias aliquid ipsa quaerat, neque autem dolore perspiciatis ipsum qui adipisci sunt, delectus ad reprehenderit ullam?</p>
 </body>
 
